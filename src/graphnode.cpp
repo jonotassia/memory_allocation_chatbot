e2f0db3a27d029c138a21734747f28db7feb5c35
Jono Tassia <jonotassia@gmail.com>
2023-03-08 19:00:48 +1100

    Fixed bugs

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -33,7 +33,7 @@ void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 
 //// STUDENT CODE
 ////
-void GraphNode::MoveChatbotHere(ChatBot chatbot)
+void GraphNode::MoveChatbotHere(ChatBot& chatbot)
 {
     *_chatBot = ChatBot(std::move(chatbot));
     _chatBot->SetCurrentNode(this);
